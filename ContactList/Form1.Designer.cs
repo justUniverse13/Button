4d7f39d571bfa3e9482d36238ff472fa6b1b7887
maxxtroflatron <maxxtroflatron@gmail.com>
2019-04-06 23:22:31 +0300

    Add project files.

---
+++ ContactList/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace ContactList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ColorComboBox1 = new System.Windows.Forms.ComboBox();
+            this.FormComboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ColorComboBox1
+            // 
+            this.ColorComboBox1.FormattingEnabled = true;
+            this.ColorComboBox1.Location = new System.Drawing.Point(85, 34);
+            this.ColorComboBox1.Name = "ColorComboBox1";
+            this.ColorComboBox1.Size = new System.Drawing.Size(121, 21);
+            this.ColorComboBox1.TabIndex = 0;
+            this.ColorComboBox1.SelectedIndexChanged += new System.EventHandler(this.ColorComboBox1_SelectedIndexChanged);
+            // 
+            // FormComboBox2
+            // 
+            this.FormComboBox2.FormattingEnabled = true;
+            this.FormComboBox2.Location = new System.Drawing.Point(641, 29);
+            this.FormComboBox2.Name = "FormComboBox2";
+            this.FormComboBox2.Size = new System.Drawing.Size(121, 21);
+            this.FormComboBox2.TabIndex = 1;
+            this.FormComboBox2.SelectedIndexChanged += new System.EventHandler(this.FormComboBox2_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightGoldenrodYellow;
+            this.button1.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.button1.Location = new System.Drawing.Point(251, 268);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(273, 88);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "RESULT";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Select Colour:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(545, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Select Form:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(340, 84);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "SUBMIT";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(866, 501);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.FormComboBox2);
+            this.Controls.Add(this.ColorComboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox ColorComboBox1;
+        private System.Windows.Forms.ComboBox FormComboBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ ContactList/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ContactList
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            AddElements();
+        }
+
+        private void ColorComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+          
+        }
+
+        private void FormComboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ButtonForm();
+            ButtonColor();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
+        private void ButtonColor()
+        {
+            switch (ColorComboBox1.SelectedIndex)
+            {
+                case 0:
+                    button1.BackColor = Color.Blue; break;
+                case 1:
+                    button1.BackColor = Color.Red; break;
+                case 2:
+                    button1.BackColor = Color.Yellow; break;
+                case 3:
+                    button1.BackColor = Color.Green; break;
+                case 4:
+                    button1.BackColor = Color.White; break;
+                case 5:
+                    button1.BackColor = Color.Black; break;
+            }
+        }
+        private void ButtonForm()
+        {
+            if (FormComboBox2.SelectedIndex == 0)
+            {
+                button1.Width = 200;
+                button1.Height = 1500;
+            }
+            else if (FormComboBox2.SelectedIndex == 1)
+            {
+                button1.Width = 200;
+                button1.Height = 200;
+            }
+            else if (FormComboBox2.SelectedIndex == 2)
+            {
+                System.Drawing.Drawing2D.GraphicsPath myPath =
+     new System.Drawing.Drawing2D.GraphicsPath();
+                myPath.AddEllipse(0, 0, button1.Width, button1.Height);
+
+                Region myRegion = new Region(myPath);
+                button1.Region = myRegion;
+            }
+        }
+        private void AddElements()
+        {
+            ColorComboBox1.Items.Add("Blue");
+            ColorComboBox1.Items.Add("Red");
+            ColorComboBox1.Items.Add("Yellow");
+            ColorComboBox1.Items.Add("Green");
+            ColorComboBox1.Items.Add("White");
+            ColorComboBox1.Items.Add("Black");
+
+            FormComboBox2.Items.Add("Rectangle");
+            FormComboBox2.Items.Add("Square");
+            FormComboBox2.Items.Add("Elipsse");
+        }
+    }
+}
